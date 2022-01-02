331275ace5cdbe9d6f00f2db91de23946c3331ad
Rubens Soares <rubens.soares@sensedia.com>
2022-01-01 21:06:14 -0300

    Created User entity.

---
+++ src/main/java/br/com/rubens/soares/api/domain/User.java
@@ -0,0 +1,27 @@
+package br.com.rubens.soares.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
