976da51b7e863f7ece4af0d7468fe6910fafd2bb
Rubens Soares <rubens.soares@sensedia.com>
2022-01-01 22:13:49 -0300

    Created User Resource findById

--- src/main/java/br/com/rubens/soares/api/resource/UserResource.java
+++ src/main/java/br/com/rubens/soares/api/resource/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.rubens.soares.api.resource;
 
 import br.com.rubens.soares.api.domain.User;
+import br.com.rubens.soares.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok(new User(1, "Rubens", "rubens@mail.com", "1234"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 
 }
